e1af1598a9ff6e85f8e81b7a3d45f372b40c1de5
Robert BuRnEr Schadek <rburners@gmail.com>
2012-05-19 23:02:21 +0200

    tester

--- tester.py
+++ tester.py
@@ -5,7 +5,9 @@ import re
 
 def globTests(): # get all tests that fit the form
 	testCaseRegex = re.compile("tests/test[0-9]+\.d")
-	return sorted([ i for i in glob.glob("tests/test[0-9][0-9]*.d") if testCaseRegex.match(i) != None])
+	testCaseNumber = re.compile("[0-9]+")
+	return sorted([ i for i in glob.glob("tests/test[0-9]*.d") if testCaseRegex.match(i) != None],
+		key = lambda i: int(testCaseNumber.search(i).group(0)))
 
 if __name__ == "__main__":
 	print(globTests())
